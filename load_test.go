a356669bea6bf284375f19ec3bc25f77a3f61196
Nikita Sivukhin <sivukhin@turso.tech>
2025-12-06 02:34:59 +0400

    add logging in test

--- load_test.go
+++ load_test.go
@@ -1,6 +1,7 @@
 package turso_libs
 
 import (
+	"path"
 	"testing"
 
 	"github.com/ebitengine/purego"
@@ -14,6 +15,36 @@ var (
 func TestLoad(t *testing.T) {
 	t.Log(libraryFilename())
 	t.Log(embeddedLibraryPath())
+
+	var list func(p string)
+	list = func(p string) {
+		file, err := libs.Open(p)
+		t.Log("open", p, err)
+		if err != nil {
+			return
+		}
+
+		stat, err := file.Stat()
+		t.Log("stat", stat, err)
+		if err != nil {
+			return
+		}
+
+		if !stat.IsDir() {
+			return
+		}
+
+		dirs, err := libs.ReadDir(p)
+		t.Log("read_dir", dirs, err)
+		if err != nil {
+			return
+		}
+
+		for _, dir := range dirs {
+			list(path.Join(p, dir.Name()))
+		}
+	}
+	list(".")
 
 	library, err := LoadTursoLibrary(LoadTursoLibraryConfig{LoadStrategy: "mixed"})
 	require.Nil(t, err)
